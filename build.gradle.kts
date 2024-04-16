dcb816062b64a1fcc36bd5cf26d8bec1fb758bd5
Arvid Smit <arvid.code@freedom.nl>
2024-04-16 19:09:29 +0200

    Remove unnecessary testing step and dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,16 +10,12 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
     implementation(kotlin("reflect"))
     implementation("org.apache.httpcomponents:httpclient:4.5.14")
     implementation("org.jsoup:jsoup:1.17.2")
 
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(21)
 }
